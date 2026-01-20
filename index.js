2435379899f25135e9df3ca5c5f5fbdfbdca0af9
Rustam <102970338+RustamSharipo@users.noreply.github.com>
2026-01-20 17:58:25 +0500

    sliders

---
+++ index.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+  var $slider = $(".offer-slider");
+  var $slider1 = $(".product");
+  var $slider2 = $(".other-products");
+  var $slider3 = $(".clients");
+
+  $slider.slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button class="slick-prev">&larr;</button>',
+    nextArrow: '<button class="slick-next">&rarr;</button>',
+  });
+  $slider1.slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-prev-1">&larr;</button>',
+    nextArrow: '<button class="slick-next-1">&rarr;</button>',
+  });
+  $slider2.slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-prev-2">&larr;</button>',
+    nextArrow: '<button class="slick-next-2">&rarr;</button>',
+  });
+  $slider3.slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+  
+  function calculateTotalPages() {
+    var slidesToShow = slick.options.slidesToShow;
+    var totalSlides = slick.slideCount;
+    return Math.ceil(totalSlides / slidesToShow);
+  }
+
+  // Обновление счетчика - показываем страницы, а не слайды
+  var totalPages = calculateTotalPages();
+  $(".total-slides").text(totalPages < 10 ? "0" + totalPages : totalPages);
+
+  $slider.on("afterChange", function (event, slick, currentSlide) {
+    // Рассчитываем текущую страницу
+    var slidesToShow = slick.options.slidesToShow;
+    var currentPage = Math.floor(currentSlide / slidesToShow) + 1;
+    
+    // Обновляем общее количество страниц (на случай изменения адаптивности)
+    var totalPages = Math.ceil(slick.slideCount / slidesToShow);
+    
+    $(".slider-counter").html(
+      (currentPage < 10 ? "0" + currentPage : currentPage) +
+        "/" +
+        (totalPages < 10 ? "0" + totalPages : totalPages),
+    );
+    
+    // Обновляем также .total-slides (на случай адаптивности)
+    $(".total-slides").text(totalPages < 10 ? "0" + totalPages : totalPages);
+  });
+  
+  // Также обновляем при изменении размера окна (адаптивность)
+  $(window).on('resize', function() {
+    setTimeout(function() {
+      var currentSlide = slick.currentSlide;
+      var slidesToShow = slick.options.slidesToShow;
+      var currentPage = Math.floor(currentSlide / slidesToShow) + 1;
+      var totalPages = Math.ceil(slick.slideCount / slidesToShow);
+      
+      $(".slider-counter").html(
+        (currentPage < 10 ? "0" + currentPage : currentPage) +
+          "/" +
+          (totalPages < 10 ? "0" + totalPages : totalPages),
+      );
+      $(".total-slides").text(totalPages < 10 ? "0" + totalPages : totalPages);
+    }, 100);
+  });
+});
